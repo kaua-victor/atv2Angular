53edfb467596c82313191ff39ce4272b12113823
kaua-victor <130488890+kaua-victor@users.noreply.github.com>
2024-12-10 16:37:44 -0300

    Add files via upload

---
+++ atv2Angular/src/app/shared/modelo/aluno.ts
@@ -0,0 +1,8 @@
+export class Aluno {
+
+    constructor(public matricula: string='',
+                public nome: string = '',
+                public idade: number = 0
+                ) {
+    }
+}
